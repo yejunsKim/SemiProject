e962f428ceb15e339b4a8c158f7f28d328ad3d41
dongdangs <dong@localhost>
2025-06-20 16:07:18 +0900

    브런치 올려요 - 신동희

---
+++ SemiProject/src/main/java/test/controller/Test.java
@@ -0,0 +1,21 @@
+package test.controller;
+
+import common.controller.BaseController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Test extends BaseController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		
+		
+		super.setRedirect(false);
+		//super.setViewPage("/WEB-INF/register.jsp");
+		super.setViewPage("/WEB-INF/error.jsp");
+
+	}
+
+}
+  
