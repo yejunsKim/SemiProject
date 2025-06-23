cb61cb03dbcd64a20931f41e310a10cc2553b812
useryejun12348888 <user@DESKTOP-ESIUD68>
2025-06-23 14:00:10 +0900

    commit

--- SemiProject/src/main/webapp/js/main/main.js
+++ SemiProject/src/main/webapp/js/main/main.js
@@ -1,9 +1,15 @@
 $(function(){
+	
+	
 	var myCarousel = document.querySelector('#carouselExampleIndicators')
 	var carousel = new bootstrap.Carousel(myCarousel, {
 	  interval: 2000,
 	  wrap: false,
 	  autoplay:true,
 	  loop:true
+	});
+	
+	myCarousel.addEventListener('slide.bs.carousel', function () {
+	  // do something...
 	})
-})
+});;
