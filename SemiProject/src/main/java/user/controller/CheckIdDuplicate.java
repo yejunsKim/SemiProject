6881018bfc0ccc508c1d11541c56bb03ec229714
dongdangs <dong@localhost>
2025-06-25 17:11:39 +0900

    register와 id, email체크 및, 공백 유무에 따른 추가 사항, css, main페이지 합치기 완료.

--- SemiProject/src/main/java/user/controller/CheckIdDuplicate.java
+++ SemiProject/src/main/java/user/controller/CheckIdDuplicate.java
@@ -1,14 +1,38 @@
 package user.controller;
 
+import org.json.JSONObject;
+
 import common.controller.BaseController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import user.model.UserDAO;
+import user.model.UserDAO_imple;
 
 public class CheckIdDuplicate extends BaseController {
-
+	private UserDAO userDAO = new UserDAO_imple();
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+		
+		String method = request.getMethod();
+		
+		if("post".equalsIgnoreCase(method)) {
+			String id = request.getParameter("id");
+			
+			boolean isExists = userDAO.checkIdDuplicate(id);
+			
+			// 자바스크립트의 {} 로 만들어줄 JSON객체
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("isExists",isExists); 
+			//{"isExists":true(false)} 형태 만들고,
+			// 문자형태인 {"isExists":true(false)} 로 만들려면 toString()
+			String json = jsonObj.toString(); 
+			System.out.println("json 형태 >> " + json);
+			  
+			request.setAttribute("json", json);
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/jsonview.jsp");
+		}
 
 	}
 
