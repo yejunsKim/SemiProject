6dc3d8b1b0b17caf1266d037e35c82c65684d89d
user <user@DESKTOP-RKHPT5N>
2025-06-26 17:23:31 +0900

    rdg7203 - ItemDetail.java 에 getHeader 추가, 주소창에 직접 입력시 메인 페이지로 이동되게끔 수정.

--- SemiProject/src/main/java/myshop/controller/ItemDetail.java
+++ SemiProject/src/main/java/myshop/controller/ItemDetail.java
@@ -18,29 +18,39 @@ public class ItemDetail extends BaseController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		String s_itemno = request.getParameter("itemno");
+		String referer = request.getHeader("Referer");
 		
-		if(s_itemno != null) {
-			int itemno = Integer.parseInt(s_itemno);
-		
-			ItemVO item = idao.selectOneItemByItemNo(itemno);
-			
-			if(item != null) {
-				request.setAttribute("item", item);
-				super.setRedirect(false);
-				super.setViewPage("/WEB-INF/shop/itemDetail.jsp");
-			}
+		if(referer == null) {
+			super.setRedirect(true);
+			super.setViewPage(request.getContextPath() + "/main.do");
+			return;	// 종료
 		}
 		
 		else {
-			String message = "등록된 상품이 없습니다.";
-			String loc = request.getContextPath()+"/main.do";
+			String s_itemno = request.getParameter("itemno");
 			
-			request.setAttribute("message", message);
-			request.setAttribute("loc", loc);
+			if(s_itemno != null) {
+				int itemno = Integer.parseInt(s_itemno);
 			
-			super.setRedirect(false);
-			super.setViewPage("/WEB-INF/msg.jsp");
+				ItemVO item = idao.selectOneItemByItemNo(itemno);
+				
+				if(item != null) {
+					request.setAttribute("item", item);
+					super.setRedirect(false);
+					super.setViewPage("/WEB-INF/shop/itemDetail.jsp");
+				}
+			}
+			
+			else {
+				String message = "등록된 상품이 없습니다.";
+				String loc = request.getContextPath()+"/main.do";
+				
+				request.setAttribute("message", message);
+				request.setAttribute("loc", loc);
+				
+				super.setRedirect(false);
+				super.setViewPage("/WEB-INF/msg.jsp");
+			}
 		}
 		
 	} 
