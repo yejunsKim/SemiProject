dfbf38594bf3d289b60f68a5e451a6a487ee8662
dongdangs <dong@localhost>
2025-06-27 17:10:05 +0900

    회원정보 창으로 이동 및 command.properties, /user/UserEdit.do=user.controller.UserEdit

---
+++ SemiProject/src/main/java/user/controller/UserEdit.java
@@ -0,0 +1,15 @@
+package user.controller;
+
+import common.controller.BaseController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserEdit extends BaseController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
