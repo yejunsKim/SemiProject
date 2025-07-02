d36c7fca46f77617c5a75373c36494d2d2625169
user <user@DESKTOP-RKHPT5N>
2025-07-02 19:30:24 +0900

    rdg - 0702 19:30

--- SemiProject/src/main/java/myshop/controller/OrderList.java
+++ SemiProject/src/main/java/myshop/controller/OrderList.java
@@ -88,11 +88,7 @@ public class OrderList extends BaseController {
 			
 			loop++;		// 1 2 3 4 5 6 7 8 9 10
 			
-			pageNo++;	// 	1  2  3  4  5  6  7  8  9 10
-						// 11 12 13 14 15 16 17 18 19 20
-						// 21 22 23 24 25 26 27 28 29 30
-						// 31 32 33 34 35 36 37 38 39 40
-						// 41 42 
+			pageNo++;	 
 			
 		}// end of while(!(loop > blockSize || pageNo > totalPage))--------------------
 		
