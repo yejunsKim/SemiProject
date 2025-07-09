64f1e888c1999091188b413cb1b37db76c0c7750
user <user@DESKTOP-RKHPT5N>
2025-07-09 14:12:15 +0900

    Merge branch 'main' of https://github.com/yejunsKim/SemiProject.git into main

---
+++ SemiProject/src/main/webapp/WEB-INF/SQL/semi_rdg.sql
@@ -0,0 +1,51 @@
+select table_name
+FROM user_tables;
+
+select *
+from order_history;
+
+select *
+from users;
+
+select *
+from order_history
+where id = 'rdg';
+
+CREATE TABLE grade (
+    gradename VARCHAR2(20) PRIMARY KEY,     -- 'bronze', 'silver', 'gold'
+    mintotalamount NUMBER NOT NULL         -- 누적 결제 금액 기준
+);
+
+INSERT INTO grade(gradename, mintotalamount) VALUES('bronze', 0);
+INSERT INTO grade(gradename, mintotalamount) VALUES('silver', 300000);
+INSERT INTO grade(gradename, mintotalamount) VALUES('gold', 1000000);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_users_grade
+FOREIGN KEY (grade) REFERENCES grade(grade_name);
+
+UPDATE users U SET U.grade = ( SELECT G.gradename 
+                               FROM grade G WHERE ( SELECT SUM(totalamount) 
+                                                    FROM order_histroy OH 
+                                                    WHERE OH.id = U.id AND OH.orderdate > sysdate - 180 
+                                                  ) >= G.mintotalamount
+                               ORDER BY G.mintotalamount DESC
+                               FETCH FIRST 1 ROWS ONLY 
+                             )
+                             WHERE U.id = 'rdg';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
