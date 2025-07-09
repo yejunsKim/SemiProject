b282dd94519b0da60b58e415c3eda94e0fb0549f
YUMIN <dbals010321@gmail.com>
2025-07-09 16:01:38 +0900

    semi_project.sql 커밋

--- SemiProject/src/main/webapp/WEB-INF/SQL/semi_project.sql
+++ SemiProject/src/main/webapp/WEB-INF/SQL/semi_project.sql
@@ -248,3 +248,23 @@ nocycle
 nocache;
 
 
+
+
+---- **** MyMVC 다이내믹웹프로젝트 에서 작업한 것 **** ----
+
+-- 오라클 계정 생성을 위해서는 SYS 또는 SYSTEM 으로 연결하여 작업을 해야 합니다. [SYS 시작] --
+show user;
+-- USER이(가) "SYS"입니다.
+
+-- 오라클 계정 생성시 계정명 앞에 c## 붙이지 않고 생성하도록 하겠습니다.
+alter session set "_ORACLE_SCRIPT"=true;
+-- Session이(가) 변경되었습니다.
+
+-- 오라클 계정명은 semi_orauser2 이고 암호는 seven 인 사용자 계정을 생성합니다.
+create user semi_orauser2 identified by seven default tablespace users; 
+-- User SEMI_ORAUSER2이(가) 생성되었습니다.
+
+-- 위에서 생성되어진 MYMVC_USER 이라는 오라클 일반사용자 계정에게 오라클 서버에 접속이 되어지고,
+-- 테이블 생성 등등을 할 수 있도록 여러가지 권한을 부여해주겠습니다.
+grant connect, resource, create view, unlimited tablespace to MYMVC_USER;
+-- Grant을(를) 성공했습니다.
